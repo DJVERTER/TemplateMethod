2cd05a07c92d213a8da83d3fe056a0e288aed033
DJVERTER <creativekira1982@gmail.com>
2023-02-28 12:44:05 +0200

    add rectangle

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -2,8 +2,9 @@
 
 var triangle = new TriangleSquare(10, 15);
 var circle = new CircleSquare(20);
+var rectangle = new RectangleSquare(2, 3);  
 
-var shapes = new ISquare[] { triangle, circle };
+var shapes = new ISquare[] { triangle, circle, rectangle };
 DisplaySquare(shapes);
 Console.ReadKey();
 
---
+++ TemplateMethod/Square/RectangleSquare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod.Square
+{
+    internal class RectangleSquare : ShapeSquare
+    {
+        public double SideA { get; init; }
+        public double SideB { get; init; }
+        public RectangleSquare(double a, double b)
+        {
+            SideA = a;
+            SideB = b;
+        }
+        protected override void CalculateShape()
+        {
+            Square = SideA * SideB;
+        }
+
+        protected override void Validate()
+        {
+            IsValid = SideA >= 0 && SideB >= 0;
+        }
+    }
+}
